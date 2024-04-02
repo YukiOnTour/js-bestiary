0030ef62e1ec8e2dd80ace4a22e1af186c1809a2
YukiOnTour <yukiontour@gmail.com>
2024-04-02 10:46:15 +0200

    Added files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Define favoriteFood variable and assign it a value
+var favoriteFood = "Hamburgers";
+
+// Display favoriteFood using document.write()
+document.write("My favorite food is: " + favoriteFood);
+
+// Display favoriteFood using document.getElementById()
+document.getElementById("favoriteFoodDisplay").innerText = favoriteFood;
